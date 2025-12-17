240eb1923cc2bace5d7d45459b02a70cc0a30bfc
devsiten <whitecrypto122@gmail.com>
2025-12-17 22:35:18 +0100

    Add treasury wallet

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: 'https://hussayn-signal-api.devsiten.workers.dev',
     NEXT_PUBLIC_SOLANA_RPC: 'https://mainnet.helius-rpc.com/?api-key=e495db18-fb79-4c7b-9750-5bf08d316aaf',
+    NEXT_PUBLIC_TREASURY_WALLET: '8eQUQeiqaroRzjLZoZtqnz8371X87WUTNdv5JRKbmLe2',
   },
 };
 
